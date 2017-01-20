6d9e4304a13a81cf6bbad6e3220f866794bf88ab
Vladimir33 <vod.vladimir33@gmail.com>
2017-01-20 16:21:36 +0300

    HW05 not completed

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,11 +1,46 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
-/**
- * gkislin
- * 02.10.2016
- */
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(name = Meal.DELETE)
+    int delete(@Param("id") int id);
+
+//    @Transactional
+//    @Modifying
+//    @Query("update Meal m set m.dateTime=?1, m.calories=?2, m.description=?3 where m.user.id=:userId")
+//    Meal saveByUserId(Meal meal, @Param("userId") int userId);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+//    @Modifying
+//    @Query("select m from Meal m where m.id=:id")
+//    Meal findOne(@Param("id") int id);
+
+    @Query("SELECT m FROM Meal m JOIN FETCH m.user u WHERE u.id = :user_id AND m.id=:id ORDER BY m.dateTime DESC")
+    Meal getByIdWithUser(@Param("id")int id, @Param("user_id")int userId);
+
+//    @Override
+//    Meal findOne(Integer id);
+
+
+    @Override
+    List<Meal> findAll(Sort sort);
+
+    List<Meal> findBetweenAndUserIdIs(LocalDateTime startDate, LocalDateTime endDate, int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.datajpa.CrudMealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,31 +17,35 @@ import java.util.List;
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
 
+    private static final Sort SORT_BY_DATE_DESC = new Sort("dateTime");
+
     @Autowired
     private CrudMealRepository crudRepository;
 
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    public Meal save(Meal meal, int userId) {
+        return crudRepository.save(meal);
+        //    return null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.getByIdWithUser(id, userId);
+        //    return null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAll(SORT_BY_DATE_DESC);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.findBetweenAndUserIdIs(startDate, endDate, userId);
     }
 }
